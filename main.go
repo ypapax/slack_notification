e320d12573ecb403d46cf069841e1bfce039cd48
Maxim Yefremov <maxYefr@gmail.com>
2020-02-07 09:47:08 +0300

    with emogies

--- main.go
+++ main.go
@@ -9,30 +9,29 @@ import (
 
 func main() {
 	logrus.SetReportCaller(true)
-	if err := func() error {
-		env := "WEB_HOOK_URL_SLACK"
-		webhookUrl := os.Getenv(env)
-		if len(webhookUrl) == 0 {
-			return errors.Errorf("no env var %+v", env)
+	for _, msg := range []string{":fearful: bad",":thumbsup: ok", ":crescent_moon: Hello from <https://github.com/ashwanthkumar/slack-go-webhook|slack-go-webhook>, a Go-Lang library to send slack webhook messages.\n<https://golangschool.com/wp-content/uploads/golang-teach.jpg|golang-img>"} {
+		if err := MessageToSlack(msg); err != nil {
+			logrus.Errorf("%+v", err)
+		} else {
+			logrus.Infof("%+v is sent", msg)
 		}
-		attachment1 := slack.Attachment{}
-		attachment1.AddField(slack.Field{Title: "Author", Value: "Ashwanth Kumar"}).AddField(slack.Field{Title: "Status", Value: "Completed"})
-		attachment1.AddAction(slack.Action{Type: "button", Text: "Book flights 🛫", Url: "https://flights.example.com/book/r123456", Style: "primary"})
-		attachment1.AddAction(slack.Action{Type: "button", Text: "Cancel", Url: "https://flights.example.com/abandon/r123456", Style: "danger"})
-		payload := slack.Payload{
-			Text:        "Hello from <https://github.com/ashwanthkumar/slack-go-webhook|slack-go-webhook>, a Go-Lang library to send slack webhook messages.\n<https://golangschool.com/wp-content/uploads/golang-teach.jpg|golang-img>",
-			Username:    "robot",
-			Channel:     "#general",
-			IconEmoji:   ":monkey_face:",
-			Attachments: []slack.Attachment{attachment1},
-		}
-		err := slack.Send(webhookUrl, "", payload)
-		if len(err) > 0 {
-			return errors.Errorf("errors in sending to slack: %+v", err)
-		}
-		return nil
-	}(); err != nil {
-		logrus.Fatalf("%+v", err)
 	}
-	logrus.Infof("sent")
+
+}
+
+func MessageToSlack(msg string) error {
+	env := "WEB_HOOK_URL_SLACK"
+	webhookUrl := os.Getenv(env)
+	if len(webhookUrl) == 0 {
+		return errors.Errorf("no env var %+v", env)
+	}
+	payload := slack.Payload{
+		Text:        msg,
+		Username:    "robot",
+	}
+	err := slack.Send(webhookUrl, "", payload)
+	if len(err) > 0 {
+		return errors.Errorf("errors in sending to slack: %+v", err)
+	}
+	return nil
 }
